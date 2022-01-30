1ff7912b5410349bd1ebf7bf914f303d411c8eab
Wiktor Gindorowicz <wiktorgindorowicz@gmail.com>
2022-01-30 17:22:43 +0100

    Add articles

--- js/script.js
+++ js/script.js
@@ -21,3 +21,38 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+function titleClickHandler(event){
+  console.log('Article was clicked!');
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const articles = document.querySelectorAll('.list titles a');
+
+for(let article of articles){
+  article.addEventListener('click', titleClickHandler);
+}
+
+const activeArticles = document.querySelectorAll('.list titles a.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+
