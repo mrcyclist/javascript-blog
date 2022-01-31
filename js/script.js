5c1b91069c8b8575bfaa150cd278e06935eb4063
Wiktor Gindorowicz <wiktorgindorowicz@gmail.com>
2022-01-31 22:43:02 +0100

    Moduł 5.4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -16,19 +17,35 @@ function titleClickHandler(event){
 
   console.log('clickedElement:', clickedElement);
 
+  clickedElement.classList.add('active');
+
   /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log (targetArticle);
+
   /* add class 'active' to the correct article */
+  
+  targetArticle.classList.add('active');
+
 }
 
-const links = document.querySelectorAll('.titles a');
+  const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
 }
 
-clickedElement.add('active');
