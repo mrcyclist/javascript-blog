e1aa59e2b0e5952fbec23cf9bc592154c0a8ecba
Wiktor Gindorowicz <wiktorgindorowicz@gmail.com>
2022-02-01 14:21:40 +0100

    End of the module 5

--- js/script.js
+++ js/script.js
@@ -35,17 +35,62 @@ function titleClickHandler(event){
 
   const targetArticle = document.querySelector(articleSelector);
 
-  console.log (targetArticle);
+  console.log(targetArticle);
 
   /* add class 'active' to the correct article */
   
   targetArticle.classList.add('active');
 
-}
+};
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+  
+  for(let article of articles){
+
+    /* get the article id */
+  
+  const articleId = article.getAttribute('id');
+
+    /* find the title element */
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+  
+
+    /* create HTML of the link */
+
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+  console.log(linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
 
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
+  }
 }
 
+generateTitleLinks();
